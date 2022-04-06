fc254e07afb6918a9849b36202fe067119d60a28
EoBryanDev <mauriciobryan1998@gmail.com>
2022-04-06 20:59:19 -0300

    Simple Create form - w/ error

--- SalesWeb-CSharp/Controllers/SellersController.cs
+++ SalesWeb-CSharp/Controllers/SellersController.cs
@@ -16,5 +16,17 @@ namespace SalesWeb_CSharp.Controllers
 
             return View(list);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- SalesWeb-CSharp/Services/SellerService.cs
+++ SalesWeb-CSharp/Services/SellerService.cs
@@ -13,5 +13,12 @@
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj)
+        {
+            
+            _context.Add(obj);
+            
+            _context.SaveChanges();
+        }
     }
 }
