ad322d2e488c78097354a246dd601696388927ac
EoBryanDev <mauriciobryan1998@gmail.com>
2022-04-06 21:58:14 -0300

    Seller details and eager loading

--- SalesWeb-CSharp/Services/SellerService.cs
+++ SalesWeb-CSharp/Services/SellerService.cs
@@ -1,4 +1,9 @@
-﻿namespace SalesWeb_CSharp.Services;
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace SalesWeb_CSharp.Services;
+
+
 
 public class SellerService
 {
@@ -23,7 +28,7 @@ public class SellerService
 
     public Seller FindById(int id)
     {
-        return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
     }
     public void Remove(int id)
     {
