f625ddf2d208df3c197b59f5a27e920e348aa243
EoBryanDev <mauriciobryan1998@gmail.com>
2022-04-06 21:07:13 -0300

    Foreign key not null(referential integrity) - error solved

--- SalesWeb-CSharp/Services/SellerService.cs
+++ SalesWeb-CSharp/Services/SellerService.cs
@@ -15,7 +15,7 @@
         }
         public void Insert(Seller obj)
         {
-            
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             
             _context.SaveChanges();
